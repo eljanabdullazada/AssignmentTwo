e59085fdc66612a7eeffca72207c172be775012f
Elcan <elcan@Elcans-MacBook-Air-2.local>
2023-12-01 04:03:46 +0400

    Complete header and footer

--- second try/script.js
+++ second try/script.js
@@ -1,32 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetchProducts();
+document.addEventListener("DOMContentLoaded", () => {
+  fetchProducts();
 });
 
 async function fetchProducts() {
-    try {
-        const response = await fetch('https://dummyjson.com/products');
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        displayProducts(data.products);
-    } catch (error) {
-        console.error('Error occured: ', error);
+  try {
+    const response = await fetch("https://dummyjson.com/products");
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+    const data = await response.json();
+    displayProducts(data.products);
+  } catch (error) {
+    console.error("Error occured: ", error);
+  }
 }
 
 function displayProducts(products) {
-    const container = document.getElementById('product-List');
-    products.forEach(product => {
-        const productElement = document.createElement('div');
-        productElement.classList.add('product'); 
-        productElement.innerHTML = `
-            <h2>${product.title}</h2>
+  const container = document.getElementById("product-List");
+  products.forEach((product) => {
+    const productElement = document.createElement("div");
+    productElement.classList.add("product");
+    productElement.innerHTML = `
+            <h5 id="item-category" >${product.category}</h5>
+            <h3>${product.title}</h3>
             <img src="${product.thumbnail}" alt="${product.title}" />
             <p>${product.description}</p>
             <p>Price: $${product.price}</p>
+            <p>Discount: $${product.discountPercentage}</p>
+            <p>Available item: ${product.stock}</p>
         `;
-        container.appendChild(productElement);
-    });
+    container.appendChild(productElement);
+  });
 }
-
